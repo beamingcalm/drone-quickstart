a840acca72aa0c62ef47ca372808bc4350c1c25e
beamingcalm <61388473+beamingcalm@users.noreply.github.com>
2023-05-27 18:30:06 +0300

    Update script.py

--- script.py
+++ script.py
@@ -2,19 +2,19 @@
 This script demonstrates calculating.
 """
 
-def calculate_average(nums):
+def calculate_average(num_list):
     """
     Calculate the average of a list of numbers.
 
     Args:
-        numbers (list): List of numbers.
+        num_list (list): List of numbers.
 
     Returns:
         float: Average value.
     """
-    if len(nums) == 0:
+    if len(num_list) == 0:
         return None
-    return sum(nums) / len(nums)
+    return sum(num_list) / len(num_list)
 
 def is_even(number):
     """
@@ -28,25 +28,25 @@ def is_even(number):
     """
     return number % 2 == 0
 
-def count_even_numbers(nums):
+def count_even_numbers(num_list):
     """
     Count the number of even numbers in a list.
 
     Args:
-        numbers (list): List of numbers.
+        num_list (list): List of numbers.
 
     Returns:
         int: Count of even numbers.
     """
     count = 0
-    for number in nums:
+    for number in num_list:
         if is_even(number):
             count += 1
     return count
 
 if __name__ == "__main__":
-    nums = [1, 2, 3, 4, 5, 6]
-    average = calculate_average(nums)
+    numbers = [1, 2, 3, 4, 5, 6]
+    average = calculate_average(numbers)
     print(f"Average: {average}")
-    EVEN_COUNT = count_even_numbers(nums)
+    EVEN_COUNT = count_even_numbers(numbers)
     print(f"Number of even numbers: {EVEN_COUNT}")
